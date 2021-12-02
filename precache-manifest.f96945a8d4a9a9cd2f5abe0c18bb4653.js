c67e8aa558cd33121a0ac5c31f2ae5ce05754987
Luis Enrique Henao <lhenao12@gmail.com>
2021-12-01 23:20:08 -0500

    Updates

---
+++ precache-manifest.f96945a8d4a9a9cd2f5abe0c18bb4653.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "592c047c4f715ba71d19fdab0cbbdfa3",
+    "url": "/React-2021/index.html"
+  },
+  {
+    "revision": "ec82521b3117fbabaf90",
+    "url": "/React-2021/static/css/main.4bcf1253.chunk.css"
+  },
+  {
+    "revision": "4feb26bdf9c21d28ef66",
+    "url": "/React-2021/static/js/2.db0edaa9.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/React-2021/static/js/2.db0edaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec82521b3117fbabaf90",
+    "url": "/React-2021/static/js/main.d0b6277b.chunk.js"
+  },
+  {
+    "revision": "2e9f8c614da5ad6b8f46",
+    "url": "/React-2021/static/js/runtime-main.4cf6ac0e.js"
+  },
+  {
+    "revision": "d7efe4df58373e6954cedaf5282da551",
+    "url": "/React-2021/static/media/checktodo.d7efe4df.svg"
+  },
+  {
+    "revision": "76bf3982b33236b5d0a4f1f91d05d136",
+    "url": "/React-2021/static/media/deletetodo.76bf3982.svg"
+  }
+]);
